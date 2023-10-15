8d7b48543b0f93ab18dfe31a0571efe827531ff4
sebgaw12 <sebastian.gawrys1@gmail.com>
2023-10-15 19:16:38 +0200

    refactor, adding new modules

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
+import {draw, snakeMove} from "./SnakeDetails.js";
+import {snake} from "./gameObjects.js";
+
 let lastRenderTime = 0
 const board = document.querySelector('#game-board')
-    
+
 const initGame = (currentTime) => {
     window.requestAnimationFrame(initGame)
     
@@ -8,29 +11,8 @@ const initGame = (currentTime) => {
     if (secondSinceLastRender < 1 / snake.speed) return
     
     lastRenderTime = currentTime
-    draw()
+    snakeMove()
+    draw(board)
 }   
-
-let draw = () => {
-    board.innerHTML = ''
-    createSnake(board)
-}
-
-let createSnake = (gameBoard) => {
-    snake.snakeBody.forEach(segment => {
-        const snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = segment.y
-        snakeElement.style.gridColumnStart = segment.x
-        snakeElement.classList.add('snake')
-        gameBoard.appendChild(snakeElement)
-    })
-}
-
-let snake = {
-    speed: 1,
-    snakeBody: [{x: 11, y: 11},
-        {x: 12, y:11},
-        {x: 13, y:11}]
-}
 
 window.requestAnimationFrame(initGame)
